96c8530cc43b951fe0fe95e5c8dc2b6702ab1a00
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2021-02-17 01:44:12 +0100

    Updated the script to run with python3

--- gesturecrack.py
+++ gesturecrack.py
@@ -1,12 +1,14 @@
 #Quick and Easy Android Pattern Lock Cracker - Kieron Craggs 2014
 #This quick script will use a gesture.key file or raw text hash and compare it against a rainbow table to find the pattern used to lock the device
 #Uses GestureKey Rainbow Table
+#Webblitchy : converted the script to run with python3
 
 #****MODULES*************#
 import argparse
 import sqlite3 as lite
 import gzip
 import os
+import codecs
 #************************#
 
 parser = argparse.ArgumentParser(description = "Easy Android Pattern Lock Cracker")
@@ -17,32 +19,32 @@ args = parser.parse_args()
 
 if args.file:
     gesturekey = open (args.file,'rb').read()
-    inkey = str(gesturekey).encode('hex-codec')
+    inkey = codecs.encode(str(gesturekey),"hex_codec")
 elif args.rawhash:
     inkey = args.rawhash
 else:
-    print "Supply a valid gesture key file or raw text hash"
+    print("Supply a valid gesture key file or raw text hash")
 
 
 try:
     dbin = gzip.GzipFile('rainbow.db.gz','rb')
 except IOError:
-    print "Database archive doesn't exist, put rainbow.db.gz in current working directory"
+    print("Database archive doesn't exist, put rainbow.db.gz in current working directory")
     exit()
 else:
     dbbuf = dbin.read()
     dbin.close()
 
-    dbout = file('rainbow.db','wb')
+    dbout = open('rainbow.db','wb')
     dbout.write(dbbuf)
     dbout.close()    
 
 try:
     db = lite.connect('rainbow.db') 
 except NameError:
-    print "Nothing was given to the database to check. Check gesture input file."
+    print("Nothing was given to the database to check. Check gesture input file.")
 except IOError:
-    print "Cannot find db file."
+    print("Cannot find db file.")
 else:    
     with db:
         cur = db.cursor()
@@ -52,7 +54,7 @@ else:
         if rows:
             for row in rows:
                 result = row
-                print """   
+                print("""   
 
         The Lock Pattern code is %s
 
@@ -62,10 +64,6 @@ else:
         |3|4|5|
         |6|7|8|
 
-                """ % (row)
+                """ % row)
         else:
-            print "No match, check input."                    
-
-if os.path.exists("rainbow.db"):       
-        os.remove("rainbow.db")
-        exit()    
+            print("No match, check input.")
